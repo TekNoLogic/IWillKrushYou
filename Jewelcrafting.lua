c948520fef4b75e4b3c3188cd0dbba4fd6174e2b
tekkub :bear:  <tekkub@gmail.com>
2012-10-25 05:04:44 -0600

    New pandas prospecting data

--- Jewelcrafting.lua
+++ Jewelcrafting.lua
@@ -9,6 +9,8 @@ local WRATH_BLUE_GEMS = {36918, 36930, 36921, 36933, 36924, 36927}
 local WRATH_PURPLE_GEMS = {36919, 36931, 36922, 36934, 36925, 36928}
 local CAT_GREEN_GEMS = {52177, 52181, 52179, 52182, 52178, 52180}
 local CAT_BLUE_GEMS = {52190, 52193, 52195, 52192, 52191, 52194}
+local PANDA_GREEN_GEMS = {76136, 76130, 76134, 76137, 76133, 76135}
+local PANDA_BLUE_GEMS = {76131, 76140, 76142, 76139, 76138, 76141}
 local CUTS = {
 	[23077] = {23094, 23095, 23097, 23096, 28595},
 	[21929] = {23098, 23099, 23100, 23101, 31866, 31869},
@@ -50,6 +52,18 @@ local CUTS = {
 	[36934] = {40164, 40165, 40166, 40167, 40168, 40169, 40170, 40171, 40172, 40173, 40174, 40175, 40176, 40177, 40178, 40179},
 	[36925] = {40119, 40120, 40121, 40122},
 	[36928] = {40129, 40130, 40131, 40132, 40133, 40134, 40135, 40136, 40137, 40138, 40139, 40140, 40141, 40180, 40181, 40182},
+	[76136] = {76560, 76562, 76561, 76563, 76564},
+	[76130] = {76534, 76530, 76533, 76532, 76531, 76529, 76528, 76535, 76536, 76545, 76544, 76543, 76542, 76541, 76539, 76538, 76527, 76526, 76540, 76546, 76547},
+	[76134] = {76566, 76567, 76565, 76568, 76569},
+	[76137] = {76525, 76510, 76523, 76508, 76509, 76511, 76512, 76513, 76514, 76517, 76518, 76519, 76520, 76521, 76522, 76507, 76515, 76524},
+	[76133] = {76506, 76504, 76505, 76502},
+	[76135] = {76556, 76555, 76553, 76552, 76551, 76549, 76548, 76554, 76550, 76559, 76558, 76557, 89678, 89675},
+	[76131] = {76693, 76692, 76694, 76695, 76696},
+	[76140] = {76675, 76663, 76661, 76660, 76659, 76658, 76677, 76678, 76679, 76676, 76662, 76664, 76674, 76673, 76672, 76670, 76669, 76668, 76667, 76666, 76665, 76671},
+	[76142] = {76701, 76697, 76698, 76699, 76700},
+	[76139] = {76647, 76651, 76652, 76653, 76654, 76655, 76656, 76657, 76648, 76649, 76650, 76646, 76645, 76644, 76643, 76641, 76640, 76642},
+	[76138] = {76636, 76637, 76638, 76639},
+	[76141] = {76680, 76681, 89680, 89674, 76691, 76690, 76689, 76687, 76688, 76686, 76685, 76684, 76683, 76682},
 }
 
 local function GS(cash)
@@ -112,17 +126,46 @@ end
 
 -- Rates are the cumulative sum of all results divided by total prospects, from wowhead
 -- Since this is the number of prospects that yield the item, not the number of items
--- received from teh prospect, we have to multiply the result rate by the avearage yield
+-- received from the prospect, we have to multiply the result rate by the avearage yield
 -- It is safe to assume that every quantity has equal frequency, so 2-5x is 3.5x avg
-local origs = {}
 local   FEL_IRON_GREEN_RATE,   FEL_IRON_BLUE_RATE = 1.027, 0.060
 local ADAMANTITE_GREEN_RATE, ADAMANTITE_BLUE_RATE = 1.100, 0.195
-local   COBALT_GREEN_RATE,       COBALT_BLUE_RATE = 1.5*1.5, 0.074
-local SARONITE_GREEN_RATE,     SARONITE_BLUE_RATE = 1.1*1.5, 0.240
-local TITANIUM_GREEN_RATE,     TITANIUM_BLUE_RATE, TITANIUM_PURPLE_RATE, TITANIUM_DUST_RATE = 1.5, 0.25, 0.27, 0.58
-local   OBSIDIUM_GREEN_RATE,   OBSIDIUM_BLUE_RATE = 1.415 *1.5, 0.075
-local ELEMENTIUM_GREEN_RATE, ELEMENTIUM_BLUE_RATE = 1.082 *1.5, 0.269
-local     PYRITE_GREEN_RATE,     PYRITE_BLUE_RATE = 1.000, 0.450 *1.5
+local     COBALT_GREEN_RATE,     COBALT_BLUE_RATE = 1.5*1.5, 0.074
+local   SARONITE_GREEN_RATE,   SARONITE_BLUE_RATE = 1.1*1.5, 0.240
+local   TITANIUM_GREEN_RATE,   TITANIUM_BLUE_RATE, TITANIUM_PURPLE_RATE, TITANIUM_DUST_RATE = 1.5, 0.25, 0.27, 0.58
+local   OBSIDIUM_GREEN_RATE,   OBSIDIUM_BLUE_RATE = 1.415*1.5, 0.075
+local ELEMENTIUM_GREEN_RATE, ELEMENTIUM_BLUE_RATE = 1.082*1.5, 0.269
+local     PYRITE_GREEN_RATE,     PYRITE_BLUE_RATE = 1.000, 0.450*1.5
+local GHOST_IRON_GREEN_RATE, GHOST_IRON_BLUE_RATE, GHOST_IRON_SHARD_RATE = 1.415*1.5, 0.272, 0.802*1.5
+local   KYPARITE_GREEN_RATE,   KYPARITE_BLUE_RATE,   KYPARITE_SHARD_RATE = 1.426*1.5, 0.275*1.5, 1.000*1.5
+local function gemOutputValue(id)
+	if     id == 23424 then return gemavg(BC_GREEN_GEMS)    *   FEL_IRON_GREEN_RATE + gemavg(BC_BLUE_GEMS)    *   FEL_IRON_BLUE_RATE
+	elseif id == 23425 then return gemavg(BC_GREEN_GEMS)    * ADAMANTITE_GREEN_RATE + gemavg(BC_BLUE_GEMS)    * ADAMANTITE_BLUE_RATE
+	elseif id == 36909 then return gemavg(WRATH_GREEN_GEMS) *     COBALT_GREEN_RATE + gemavg(WRATH_BLUE_GEMS) *     COBALT_BLUE_RATE
+	elseif id == 36912 then return gemavg(WRATH_GREEN_GEMS) *   SARONITE_GREEN_RATE + gemavg(WRATH_BLUE_GEMS) *   SARONITE_BLUE_RATE
+	elseif id == 36910 then return gemavg(WRATH_GREEN_GEMS) *   TITANIUM_GREEN_RATE + gemavg(WRATH_BLUE_GEMS) *   TITANIUM_BLUE_RATE
+	elseif id == 53038 then return gemavg(CAT_GREEN_GEMS)   *   OBSIDIUM_GREEN_RATE + gemavg(CAT_BLUE_GEMS)   *   OBSIDIUM_BLUE_RATE
+	elseif id == 52185 then return gemavg(CAT_GREEN_GEMS)   * ELEMENTIUM_GREEN_RATE + gemavg(CAT_BLUE_GEMS)   * ELEMENTIUM_BLUE_RATE
+	elseif id == 52183 then return gemavg(CAT_GREEN_GEMS)   *     PYRITE_GREEN_RATE + gemavg(CAT_BLUE_GEMS)   *     PYRITE_BLUE_RATE
+	elseif id == 72092 then return gemavg(PANDA_GREEN_GEMS) * GHOST_IRON_GREEN_RATE + gemavg(PANDA_BLUE_GEMS) * GHOST_IRON_BLUE_RATE
+	elseif id == 72093 then return gemavg(PANDA_GREEN_GEMS) *   KYPARITE_GREEN_RATE + gemavg(PANDA_BLUE_GEMS) *   KYPARITE_BLUE_RATE end
+end
+
+local function sparkShardValue()
+	local eye = GetAuctionBuyout(76734)
+	if eye then return eye/10
+	else return GetAuctionBuyout(90407) or 0 end
+end
+
+-- IDs of ores that don't need extra calculations factored in
+local simpleCalcs = {
+	[23424] = true,
+	[36909] = true,
+	[36912] = true,
+	[53038] = true,
+	[52185] = true,
+}
+local origs = {}
 local OnTooltipSetItem = function(frame, ...)
 	assert(frame, "arg 1 is nil, someone isn't hooking correctly")
 
@@ -135,14 +178,13 @@ local OnTooltipSetItem = function(frame, ...)
 			local id = tonumber((link:match("item:(%d+):")))
 
 			local val = 0
-			if     id == 23424 then val = (gemavg(BC_GREEN_GEMS)  *   FEL_IRON_GREEN_RATE + gemavg(BC_BLUE_GEMS) *    FEL_IRON_BLUE_RATE) * 4
-			elseif id == 23425 then val = (gemavg(BC_GREEN_GEMS)  * ADAMANTITE_GREEN_RATE + gemavg(BC_BLUE_GEMS) *  ADAMANTITE_BLUE_RATE + 2250) * 4
-			elseif id == 36909 then val = (gemavg(WRATH_GREEN_GEMS) *   COBALT_GREEN_RATE + gemavg(WRATH_BLUE_GEMS) *   COBALT_BLUE_RATE) * 4
-			elseif id == 36912 then val = (gemavg(WRATH_GREEN_GEMS) * SARONITE_GREEN_RATE + gemavg(WRATH_BLUE_GEMS) * SARONITE_BLUE_RATE) * 4
-			elseif id == 36910 then val = (gemavg(WRATH_GREEN_GEMS) * TITANIUM_GREEN_RATE + gemavg(WRATH_BLUE_GEMS) * TITANIUM_BLUE_RATE + gemavg(WRATH_PURPLE_GEMS) * TITANIUM_PURPLE_RATE + GetAuctionBuyout(46849) * TITANIUM_DUST_RATE) * 4
-			elseif id == 53038 then val = (gemavg(CAT_GREEN_GEMS) *   OBSIDIUM_GREEN_RATE + gemavg(CAT_BLUE_GEMS) *   OBSIDIUM_BLUE_RATE) * 4
-			elseif id == 52185 then val = (gemavg(CAT_GREEN_GEMS) * ELEMENTIUM_GREEN_RATE + gemavg(CAT_BLUE_GEMS) * ELEMENTIUM_BLUE_RATE) * 4
-			elseif id == 52183 then val = (gemavg(CAT_GREEN_GEMS) *     PYRITE_GREEN_RATE + gemavg(CAT_BLUE_GEMS) *     PYRITE_BLUE_RATE + GetAuctionBuyout(52327)) * 4 end
+			local outputval = gemOutputValue(id)
+			if simpleCalcs[id] then val = outputval * 4
+			elseif id == 23425 then val = (outputval + 2250) * 4
+			elseif id == 36910 then val = (outputval + gemavg(WRATH_PURPLE_GEMS) * TITANIUM_PURPLE_RATE + GetAuctionBuyout(46849) * TITANIUM_DUST_RATE) * 4
+			elseif id == 52183 then val = (outputval + GetAuctionBuyout(52327)) * 4
+			elseif id == 72092 then val = (outputval + sparkShardValue() * GHOST_IRON_SHARD_RATE) * 4
+			elseif id == 72093 then val = (outputval + sparkShardValue() *   KYPARITE_SHARD_RATE) * 4 end
 
 			local deval = 0
 			if     id == 36909 then deval = (greengemDE() *   COBALT_GREEN_RATE + gemavg(WRATH_BLUE_GEMS) *   COBALT_BLUE_RATE)/5
