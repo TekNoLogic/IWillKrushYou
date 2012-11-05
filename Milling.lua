ef043540b83978abdb81f42da5363f5b5bc93ee0
tekkub :bear:  <tekkub@gmail.com>
2012-11-04 18:28:08 -0700

    Switch to externals

--- Milling.lua
+++ Milling.lua
@@ -1,4 +1,7 @@
 ﻿
+local myname, ns = ...
+
+
 local cpigments = {
 	[39151] = {2447, 765, 2449},
 	[39334] = {785, 2450, 2452, 3820, 2453},
@@ -13,7 +16,6 @@ local cpigments = {
 local ucpigments = {[61980] = cpigments[61979], [43103] = cpigments[39334], [43104] = cpigments[39338], [43105] = cpigments[39339], [43106] = cpigments[39340], [43107] = cpigments[39341], [43108] = cpigments[39342], [43109] = cpigments[39343]}
 local cinks  = {[61978] = 61979, [39469] = 39151, [39774] = 39334, [43116] = 39338, [43118] = 39339, [43120] = 39340, [43122] = 39341, [43124] = 39342, [43126] = 39343}
 local ucinks = {[61981] = 61980, [43115] = 43103, [43117] = 43104, [43119] = 43105, [43121] = 43106, [43123] = 43107, [43125] = 43108, [43127] = 43109}
-local ids = LibStub("tekIDmemo")
 
 local function getavg(t)
 	local count, sum = 0, 0
@@ -27,7 +29,7 @@ end
 local orig = GetAuctionBuyout
 function GetAuctionBuyout(item)
 	if not item then return end
-	local id = ids[item]
+	local id = ns.ids[item]
 	if cinks[id] then
 		local price = GetAuctionBuyout(cinks[id])
 		if price then return price * 2 end
